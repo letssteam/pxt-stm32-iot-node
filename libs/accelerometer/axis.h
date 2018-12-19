f0703d1f64965f05d13bfe251a05507ed6a891d8
Sébastien NEDJAR <nedseb@goldaire.com>
2018-12-19 16:53:41 +0200

    Correct axis configuration

--- libs/accelerometer/axis.h
+++ libs/accelerometer/axis.h
@@ -1,5 +1,5 @@
 // Override in target to change inversion of axis
 
-#define ACC_SYSTEM NORTH_EAST_UP
+#define ACC_SYSTEM SIMPLE_CARTESIAN
 #define ACC_UPSIDEDOWN true
-#define ACC_ROTATION COORDINATE_SPACE_ROTATED_0
+#define ACC_ROTATION COORDINATE_SPACE_ROTATED_180
