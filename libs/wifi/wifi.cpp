658edf42459f08be05cde61c8ba3d9bc7e70dc71
Sébastien NEDJAR <nedseb@goldaire.com>
2019-02-12 15:18:05 +0200

    complete API

--- libs/wifi/wifi.cpp
+++ libs/wifi/wifi.cpp
@@ -8,29 +8,4 @@ using namespace pxt;
 //%weight=96 icon="\uf1eb" color=#1174EE 
 namespace wifi {
 
-
-    void init(Pin tx  , Pin rx, int rate) {
-    }
-
-
-    void attach(String ssid, String password) {
-
-    }
-
- 
-    bool isAttached(String ssid) {
-        return false;
-    }
-
-    void detach(){
-    }
-
-    void send(String type, String address, int port, String message) {
-
-    }
-
-    bool sendOk() {
-        return true;
-    }
-
 }
--- libs/wifi/wifi.ts
+++ libs/wifi/wifi.ts
@@ -3,6 +3,18 @@ enum MessageType {
     TCP
 }
 
+class PacketMqtt {
+    /**
+     * receives commands.
+     */
+    public topicMqtt: string;
+    
+    /**
+     * receives the message content.
+     */
+    public messageContent: string;
+}
+
 /**
  * Support for additional wifi services.
  */
@@ -21,10 +33,10 @@ namespace wifi {
     }
 
     /**
-     * Check if we are attached to the wifi network.
+     * Check if we are connected to the wifi network.
      */
     //% weight=209
-    //% blockId=wifi_isattached block="network attached?"
+    //% blockId=wifi_is_connected block="network connected?"
     export function isAttached(ssid: string = null): boolean {
         return false;
     }
@@ -58,5 +70,108 @@ namespace wifi {
     //% blockId=wifi_sendOk block="send success?"
     export function sendOk(): boolean {
         return false;
+    }
+
+    /**
+     * The HTTP get request.url(string):URL:time(ms): private long maxWait
+     * @param time set timeout, eg: 10000
+    */
+    //%subcategory=HTTP
+    //% weight=79
+    //% blockId=wifi_http_get
+    //% block="HTTP GET|url %url|timeout(ms) %time"
+    //% advanced=false
+    export function sendHttpGet(url: string, time: number): void {
+    }
+
+    /**
+     * The HTTP post request.url(string): URL; content(string):content
+     * time(ms): private long maxWait
+     * @param time set timeout, eg: 10000
+    */
+    //%subcategory=HTTP
+    //% weight=78
+    //% blockId=wifi_http_post
+    //% block="HTTP POST|url %url|content %content|timeout(ms) %time"
+    export function sendHttpPost(url: string, content: string, time: number): void { 
+    }
+
+    /**
+     * The HTTP put request.url(string): URL; content(string):content
+     * time(ms): private long maxWait
+     * @param time set timeout, eg: 10000
+    */
+    //%subcategory=HTTP
+    //% weight=78
+    //% blockId=wifi_http_put
+    //% block="HTTP PUT|url %url|content %content|timeout(ms) %time"
+    export function sendHttpPut(url: string, content: string, time: number): void { 
+    }
+
+    /**
+     * The HTTP delete request.url(string): URL; content(string):content
+     * time(ms): private long maxWait
+     * @param time set timeout, eg: 10000
+    */
+    //%subcategory=HTTP
+    //% weight=78
+    //% blockId=wifi_http_delete
+    //% block="HTTP DELETE|url %url|content %content|timeout(ms) %time"
+    export function sendHttpDelete(url: string, content: string, time: number): void { 
+    }
+
+    //% subcategory=HTTP
+    //% blockId=wifi_http_on_received_response block="on HTTP-Response received" 
+    //% blockGap = 16
+    //% weight=39	
+    export function onReceivedHttpResponse(cb: (code: number, message: string) => void): void {
+        
+    }
+
+
+    //%subcategory=IoT Services
+    //% blockId=wifi_set_thingspeak
+	//% block="Send ThingSpeak|key %key|field1 %field1|field2 %field2"
+	//% weight=70	
+	//% blockGap=7	
+    export function sendThingspeak(key: string, field1: number, field2: number): void {
+    }
+    
+    //%subcategory=IoT Services
+    //% blockId=wifi_set_ifttt
+	//% block="Send IFTTT|key %key|event_name %event|value1 %value1|value2 %value2"
+	//% weight=60	
+    export function sendIFTTT(key: string, eventname: string, value1: number, value2: number): void {
+    }
+
+	//%subcategory=MQTT
+	//% blockId=wifi_general_mqtt
+	//% block="MQTT connect|server %host|port %port|client id %clientId|username %username|password %pwd"
+	//% weight=43
+	//% blockGap=16	
+    export function connectMQTT(host: string, port: string, clientId: string, username: string, pwd: string): void {
+    }
+	
+	//%subcategory=MQTT
+    //%blockId=wifi_mqtt_publish
+    //% block="MQTT publish|topic %topic|payload %payload"
+	//% weight=42	
+	//% blockGap=16	
+    export function mqttPublish(topic: string, payload: string): void {
+    }	
+	
+	//%subcategory=MQTT
+    //%blockId=wifi_mqtt_subscribe block="MQTT subscribe|topic %topic"
+    //% blockGap = 16
+	//% weight=41	
+    export function mqttSubscribe(topic: string): void {
+    }
+
+    //% subcategory=MQTT
+    //% blockId=wifi_mqtt_on_received_message block="on MQTT received" 
+    //% blockGap = 16
+    //% weight=39	
+    export function onReceivedMqttMessage(cb: (topic: string, payload: string) => void): void {
+        
     }
 }
